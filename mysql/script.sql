1cee5aae13e7df0479ceb546733f682418d8a199
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-04-15 13:41:56 +0530

    Update and rename script.sql to mysql_script.sql

--- mysql/script.sql
+++
@@ -1,8 +0,0 @@
-CREATE MYSQL DATABASE AND TABLE
-CREATE DATABASE employees_db;
- USE employees_db;
- CREATE TABLE EMPLOYEES (emp_id INT, emp_name VARCHAR(200),
-emp_contact VARCHAR(11), emp_add VARCHAR(250));
- INSERT INTO EMPLOYEES (emp_id, emp_name, emp_contact,
-emp_add) VALUES (1, “Jagriti”, "1234567890", “Gorakhpur”);
- SELECT * FROM EMPLOYEES;
